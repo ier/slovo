1628e8729019ab7bbb98606c31095887299fca36
ier <ivan.erakhtin@gmail.com>
2022-09-06 22:45:53 +0300

    Perform refactoring: improve namings

--- src/slovo/core.clj
+++ src/slovo/core.clj
@@ -4,60 +4,65 @@
   (:gen-class))
 
 
-(def ^:private hundreds ["сто"
-                         "двести"
-                         "триста"
-                         "четыреста"
-                         "пятьсот"
-                         "шестьсот"
-                         "семьсот"
-                         "восемьсот"
-                         "девятьсот"])
+(def ^:private hundreds
+  ["сто"
+   "двести"
+   "триста"
+   "четыреста"
+   "пятьсот"
+   "шестьсот"
+   "семьсот"
+   "восемьсот"
+   "девятьсот"])
 
 
-(def ^:private tens     ["двадцать"
-                         "тридцать"
-                         "сорок"
-                         "пятьдесят"
-                         "шестьдесят"
-                         "семьдесят"
-                         "восемьдесят"
-                         "девяносто"])
+(def ^:private tens
+  ["двадцать"
+   "тридцать"
+   "сорок"
+   "пятьдесят"
+   "шестьдесят"
+   "семьдесят"
+   "восемьдесят"
+   "девяносто"])
 
 
-(def ^:private units    ["три"
-                         "четыре"
-                         "пять"
-                         "шесть"
-                         "семь"
-                         "восемь"
-                         "девять"
-                         "десять"
-                         "одиннадцать"
-                         "двенадцать"
-                         "тринадцать"
-                         "четырнадцать"
-                         "пятнадцать"
-                         "шестнадцать"
-                         "семнадцать"
-                         "восемнадцать"
-                         "девятнадцать"])
+(def ^:private units
+  ["три"
+   "четыре"
+   "пять"
+   "шесть"
+   "семь"
+   "восемь"
+   "девять"
+   "десять"
+   "одиннадцать"
+   "двенадцать"
+   "тринадцать"
+   "четырнадцать"
+   "пятнадцать"
+   "шестнадцать"
+   "семнадцать"
+   "восемнадцать"
+   "девятнадцать"])
 
 
-(def ^:private sections ["тысяч"
-                         "миллион"
-                         "миллиард"
-                         "триллион"
-                         "квадриллион"
-                         "квинтилион"
-                         "секстиллион"
-                         "септилион"
-                         "октилион"
-                         "нониллион"
-                         "дециллион"])
+(def ^:private sections
+  ["тысяч"
+   "миллион"
+   "миллиард"
+   "триллион"
+   "квадриллион"
+   "квинтилион"
+   "секстиллион"
+   "септилион"
+   "октилион"
+   "нониллион"
+   "дециллион"])
 
 
-(defn- vec->str [v]
+(defn- vec->str
+  [v]
   (->> v
        (filter #(not (blank? %)))
        (interpose " ")
@@ -78,40 +83,50 @@
         (and (>= u 3) (<= u 9)) (units (- u 3)))
 
       :else
-      (let [r0 (when (pos? h) (hundreds (dec h)))
+      (let [h* (when (pos? h) (hundreds (dec h)))
             s (when (pos? index) (sections (dec index)))]
         (cond
           (= t 1)
-          (let [r1 (units (- (mod number 100) 3))
-                r (vec->str [r0 r1 s])]
-            (if (> index 1) (str r "ов") r))
+          (let [t* (units (- (mod number 100) 3))
+                res (vec->str [h* t* s])]
+            (if (> index 1)
+              (str res "ов")
+              res))
 
           :else
-          (let [r1 (when (>= t 2) (tens (- t 2)))
-                r2 (when (and (>= u 3) (<= u 9)) (units (- u 3)))
-                r (cond
-                    (= u 1)
-                    (if (= index 1)
-                      (vec->str [r0 r1 "одна"])
-                      (vec->str [r0 r1 (if feminine-numeral-mode "одна" "один")]))
+          (let [t* (when (>= t 2) (tens (- t 2)))
+                u* (when (and (>= u 3) (<= u 9)) (units (- u 3)))
+                res (cond
+                      (= u 1)
+                      (if (= index 1)
+                        (vec->str [h* t* "одна"])
+                        (vec->str [h* t* (if feminine-numeral-mode "одна" "один")]))
 
-                    (= u 2)
-                    (if (= index 1)
-                      (vec->str [r0 r1 "две"])
-                      (vec->str [r0 r1 (if feminine-numeral-mode "две" "два")]))
+                      (= u 2)
+                      (if (= index 1)
+                        (vec->str [h* t* "две"])
+                        (vec->str [h* t* (if feminine-numeral-mode "две" "два")]))
 
-                    (or (and (>= u 3) (<= u 9)) (= u 0))
-                    (vec->str [r0 r1 r2]))
-                rs (vec->str [r s])]
+                      (or (and (>= u 3) (<= u 9)) (= u 0))
+                      (vec->str [h* t* u*]))
+                result (vec->str [res s])]
             (cond
               (= u 1)
-              (if (= index 1) (str rs "а") rs)
+              (if (= index 1)
+                (str result "а")
+                result)
 
               (and (>= u 2) (<= u 4))
-              (if (= index 1) (str rs "и") (if (> index 1) (str rs "а") rs))
+              (if (= index 1)
+                (str result "и")
+                (if (> index 1)
+                  (str result "а")
+                  result))
 
               (or (and (>= u 5) (<= u 9)) (= u 0))
-              (if (> index 1) (str rs "ов") rs))))))))
+              (if (> index 1)
+                (str result "ов")
+                result))))))))
 
 
 (defn- categories
@@ -145,13 +160,15 @@
           trim))))
 
 
-(defn words [number]
+(defn words
+  [number]
   (->> number
        in-words
        capitalize))
 
 
-(defn- rubles [input]
+(defn- rubles
+  [input]
   (if (zero? input)
     "рублей"
     (let [sub-int (fn [value from to]
@@ -170,7 +187,8 @@
           :else "рублей")))))
 
 
-(defn- parse-money [number]
+(defn- parse-money
+  [number]
   (cond
     (integer? number) {:whole number
                        :fractional 0}
@@ -186,7 +204,8 @@
                        :fractional fractional})))
 
 
-(defn- kopecks [input]
+(defn- kopecks
+  [input]
   (let [tens (int (/ input 10))
         units (- input (* tens 10))]
     (if (or (and (> tens 1) (zero? units))
@@ -198,7 +217,8 @@
         (contains? #{0 5 6 7 8 9} units) "копеек"))))
 
 
-(defn money [number]
+(defn money
+  [number]
   (let [{:keys [whole fractional]} (parse-money number)
         whole-words (words whole)
         ruble (rubles whole)
